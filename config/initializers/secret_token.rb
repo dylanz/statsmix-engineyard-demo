3ec26cb16a7b7db85868609e60c79ef735568683
Tom Markiewicz <tmarkiewicz@gmail.com>
2012-07-16 19:59:50 -0600

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StatsmixEngineyardDemo::Application.config.secret_token = '833f7f0b1e6935a419f859df6829261d53dd4aa97bb25dc0fdc5fa4b83c827ab1acd7634be131065cfd5629ce565b6fb81a47316a5726378c8255ba5e59723a3'
